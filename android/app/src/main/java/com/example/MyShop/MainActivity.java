7f770caf4bc3ece741a2a0f1e1384a554ea82682
Divyansh Bansal <divyanshbansal04@gmail.com>
2020-08-05 09:17:32 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/MyShop/MainActivity.java
@@ -0,0 +1,9 @@
+package com.example.MyShop;
+
+import io.flutter.embedding.android.FlutterActivity;
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+}
